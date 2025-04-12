c4851f0ec3787bb767ccd2f1d58aaa9c54e41cb1
Onner <FelexOTV@yandex.ru>
2025-04-12 16:03:33 +0300

    Добавлены либы

---
+++ TrafficLights/TrafficLightController.cpp
@@ -0,0 +1,3 @@
+#include "TrafficLightController.h"
+
+TrafficLightController::TrafficLightController() {}
---
+++ TrafficLights/TrafficLightController.h
@@ -0,0 +1,10 @@
+#ifndef TRAFFICLIGHTCONTROLLER_H
+#define TRAFFICLIGHTCONTROLLER_H
+
+class TrafficLightController
+{
+public:
+    TrafficLightController();
+};
+
+#endif // TRAFFICLIGHTCONTROLLER_H
